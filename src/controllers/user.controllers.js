e857a4e37090d0f8b72e268cb019b558d9ceef3d
adil <adil.business4064@gmail.com>
2024-08-15 01:52:55 +0530

    postman integration

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -24,17 +24,23 @@ const registerUser = aysncHandler(async (req, res) => {
         throw new ApiError(400, "All fields are required");
     }
     
-    const existedUser=User.findOne({
+    const existedUser=await User.findOne({
         $or: [{ username }, { email }]
     })
     if (existedUser) {
         throw new ApiError(409, "email or username already registered");
     }
 
+    //console.log(req.files)
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required");
+    }
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0) {
+        coverImageLocalPath = req.files.coverImage[0].path;
     }
 
     const avatar = await uploadOnCloudinary(avatarLocalPath);
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -2,7 +2,7 @@ import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
 cloudinary.config({
-    cloud_name: process.enve.CLOUDINARY_CLOUD_NAME,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
@@ -15,7 +15,8 @@ const uploadOnCloudinary = async (localFilePath) => {
             resource_type: "auto",
         })
         //file has been successfully uploaded on the cloudinary
-        console.log("file is successfully uploaded", response.url);
+        //console.log("file is successfully uploaded", response.url);
+        fs.unlinkSync(localFilePath);
         return response;
     } catch (error) {
         //remove the locally saved temporary file as the upload operations got failed
